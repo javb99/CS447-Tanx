eb90f6f032aa95b7bd2de06c627e0f72bbaaaa86
Joseph Van Boxtel <joseph.vanboxtel@wsu.edu>
2020-11-13 14:45:38 -0800

    Add a world class with terrrain stored in a grid.

--- Tanx/src/Tanx.java
+++ Tanx/src/Tanx.java
@@ -12,79 +12,78 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
-  /**
-   * A Simple Game of Tanx. (Skeleton leveraged from Bounce game.)
-   *
-   * The game has three states: StartUp, Playing, and GameOver, the game
-   * progresses through these states based on the user's input and the events that
-   * occur. Each state is modestly different in terms of what is displayed and
-   * what input is accepted.
-   *
-   *
-   *
-   * Skeleton Code Copied from Bounce by wallaces
-   * @author Matthew Scofield
-   *
-   */
-  public class Tanx extends StateBasedGame {
+/**
+ * A Simple Game of Tanx. (Skeleton leveraged from Bounce game.)
+ *
+ * The game has three states: StartUp, Playing, and GameOver, the game
+ * progresses through these states based on the user's input and the events that
+ * occur. Each state is modestly different in terms of what is displayed and
+ * what input is accepted.
+ *
+ *
+ *
+ * Skeleton Code Copied from Bounce by wallaces
+ * @author Matthew Scofield
+ *
+ */
+public class Tanx extends StateBasedGame {
 
-    public static final int STARTUPSTATE = 0;
-    public static final int PLAYINGSTATE = 1;
-    public static final int GAMEOVERSTATE = 2;
+	public static final int STARTUPSTATE = 0;
+	public static final int PLAYINGSTATE = 1;
+	public static final int GAMEOVERSTATE = 2;
 
-    //ex: public static final String BALL_BALLIMG_RSC = "bounce/resource/ball.png";
-    public static final String BASIC_CANNON_SPRITE = "resource/cannon1.png";
+	public static final String BASIC_CANNON_SPRITE = "resource/cannon1.png";
 
-    public final int ScreenWidth;
-    public final int ScreenHeight;
+	public final int ScreenWidth;
+	public final int ScreenHeight;
 
-    /**
-     * Create the Keep Away frame, saving the width and height for later use.
-     *
-     * @param title
-     *            the window's title
-     * @param width
-     *            the window's width
-     * @param height
-     *            the window's height
-     */
-    public Tanx(String title, int width, int height) {
-      super(title);
-      ScreenHeight = height;
-      ScreenWidth = width;
-      Entity.setCoarseGrainedCollisionBoundary(Entity.CIRCLE);
+	/**
+	 * Create the Keep Away frame, saving the width and height for later use.
+	 *
+	 * @param title
+	 *            the window's title
+	 * @param width
+	 *            the window's width
+	 * @param height
+	 *            the window's height
+	 */
+	public Tanx(String title, int width, int height) {
+		super(title);
+		ScreenHeight = height;
+		ScreenWidth = width;
 
-    }
+	}
 
 
-    @Override
-    public void initStatesList(GameContainer container) throws SlickException {
-     // addState(new StartUpState());
-     // addState(new GameOverState());
-      addState(new PlayingState());
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		//      addState(new StartUpState());
+		//      addState(new GameOverState());
+		addState(new PlayingState());
 
-      // the sound resource takes a particularly long time to load,
-      // we preload it here to (1) reduce latency when we first play it
-      // and (2) because loading it will load the audio libraries and
-      // unless that is done now, we can't *disable* sound as we
-      // attempt to do in the startUp() method.
-      //ex: ResourceManager.loadSound(BANG_EXPLOSIONSND_RSC);
+		// the sound resource takes a particularly long time to load,
+		// we preload it here to (1) reduce latency when we first play it
+		// and (2) because loading it will load the audio libraries and
+		// unless that is done now, we can't *disable* sound as we
+		// attempt to do in the startUp() method.
+		//ex: ResourceManager.loadSound(BANG_EXPLOSIONSND_RSC);
 
-      // preload all the resources to avoid warnings & minimize latency...
-      //ex: ResourceManager.loadImage(BALL_BALLIMG_RSC);
+		// preload all the resources to avoid warnings & minimize latency...
+		//ex: ResourceManager.loadImage(BALL_BALLIMG_RSC);
 
-    }
+	}
 
-    public static void main(String[] args) {
-      AppGameContainer app;
-      try {
-        app = new AppGameContainer(new Tanx("Tanx", 800, 600));
-        app.setDisplayMode(800, 600, false);
-        app.setVSync(true);
-        app.start();
-      } catch (SlickException e) {
-        e.printStackTrace();
-      }
+	public static void main(String[] args) {
+		AppGameContainer app;
+		try {
+			int length = World.tileLength * 20;
+			app = new AppGameContainer(new Tanx("Tanx", length, length));
+			app.setDisplayMode(length, length, false);
+			app.setVSync(true);
+			app.start();
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
 }
