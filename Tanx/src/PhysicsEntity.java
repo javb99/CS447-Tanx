63c421edac7f3489ad24a9d536d1ead0a7d49620
Matthew Scofield <matthew.scofield2@wsu.edu>
2020-12-15 13:43:10 -0800

    Bugfix: Entities now settle on the ground and don't fall through

--- Tanx/src/PhysicsEntity.java
+++ Tanx/src/PhysicsEntity.java
@@ -25,8 +25,7 @@ public class PhysicsEntity extends Entity {
   
   /**
     Check for collision with the terrain. This method is specific to each entity because of
-    differing entity shapes. The default implementation checks the coarse-grained bounding circle
-    before delegating to shouldResolveFineGrainedTerrainCollision.
+    differing entity shapes. The default implementation checks the coarse-grained bounding circle.
    */
   public boolean shouldResolveTerrainCollision(Terrain terrain, int delta) {
     
@@ -34,24 +33,8 @@ public class PhysicsEntity extends Entity {
     float radius = getCoarseGrainedRadius();
     
     boolean isCoarseCollision = terrain.checkCircularCollision(position, radius);
-    if (!isCoarseCollision) { return false; }
-    
-    return shouldResolveFineGrainedTerrainCollision(terrain, delta);
-  }
-  
-  /// Default implementation only supports rectangles, everything else uses the coarse-grained check.
-  public boolean shouldResolveFineGrainedTerrainCollision(Terrain terrain, int delta) {
-    boolean hasRectangle = false;
-    LinkedList<Shape> shapes = getShapes();
-    for(Shape s : shapes) {
-      if (s.getPointCount() == 4) {
-        hasRectangle = true;
-        if(terrain.checkRectangularCollision(new Vector(s.getMinX(), s.getMinY()), new Vector(s.getMaxX(), s.getMaxY()))) {
-          return true;
-        }
-      }
-    }
-    return !hasRectangle;
+
+    return isCoarseCollision;
   }
 
   protected void setVelocity(Vector v){velocity = v;}
