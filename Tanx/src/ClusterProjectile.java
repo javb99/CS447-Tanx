a6f62e3a08252a1dc309ddc94ebed95b889d7e7d
Matthew Scofield <matthew.scofield2@wsu.edu>
2020-12-15 14:12:55 -0800

    Cluster balancing

--- Tanx/src/ClusterProjectile.java
+++ Tanx/src/ClusterProjectile.java
@@ -7,10 +7,9 @@ import java.util.function.Consumer;
 public class ClusterProjectile extends Projectile{
   public static int MINI_BOMB_DAMAGE = 20;
   public static int MINI_BOMB_RADIUS = 30;
-  public static float MINI_BOMB_SPEED = .2f;
+  public static float MINI_BOMB_SPEED = .5f;
   public static int CLUSTER_SPLIT_TIME = 1000;
-  public static int NUM_MINI_BOMBS = 4;
-  public static Vector NORM = new Vector(0, 1).setLength(MINI_BOMB_SPEED);
+  public static Vector NORM = new Vector(0, -1).setLength(MINI_BOMB_SPEED);
 
 
   private int splitTime;
@@ -31,20 +30,24 @@ public class ClusterProjectile extends Projectile{
     super.update(delta);
     splitTime -= delta;
     if (splitTime <= 0) {
-      explode();
+      explode(getVelocity());
     }
   }
 
   @Override
   public void explode() {
-    Random rand = new Random();
-    int upperBound = 360;
     super.explode();
-    for (int i = 0; i < NUM_MINI_BOMBS; i++) {
-      Vector velocity = NORM;
-      velocity = velocity.rotate(rand.nextInt(upperBound));
+    explode(NORM);
+  }
 
-      MiniBomb newBomb = new MiniBomb(getX(), getY(), velocity.add(getVelocity()), MINI_BOMB_RADIUS, MINI_BOMB_DAMAGE, this);
+  public void explode(Vector angleVector) {
+    double angles[] = new double[]{30, 60, -30, -60, 0};
+    super.explode();
+    for (double angle : angles) {
+      Vector velocity = angleVector;
+      velocity = velocity.rotate(angle);
+
+      MiniBomb newBomb = new MiniBomb(getX(), getY(), velocity, MINI_BOMB_RADIUS, MINI_BOMB_DAMAGE, this);
       miniBombs.add(newBomb);
 
       float offset = getCoarseGrainedRadius();
